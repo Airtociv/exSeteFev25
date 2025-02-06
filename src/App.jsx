b4eb3d7fbce2b1b7f5fa30644c9c82b3f44af3e8
Jackson <poekyl@vivaldi.net>
2025-02-06 11:32:08 -0300

    exercicio dia 6

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src= "/d4.jpg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://vite.dev" target="_blank">
+          <img src="/d6.jpg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://vite.dev" target="_blank">
+          <img src="/d8.jpg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://vite.dev" target="_blank">
+          <img src="/d12.jpg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://vite.dev" target="_blank">
+          <img src="/d20.jpg" className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur
+        magni pariatur provident rerum ea est, odio deserunt dolorem voluptatem
+        mollitia placeat quis odit porro eaque cumque asperiores aliquam officia
+        consequuntur.
+      </p>
+    </>
+  );
+}
+
+export default App;
