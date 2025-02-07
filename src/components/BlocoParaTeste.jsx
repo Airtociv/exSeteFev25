368a40a3e179e0fb4d0ad570be9b2d6b483dea0e
Jackson <poekyl@vivaldi.net>
2025-02-07 09:54:51 -0300

    first commit

---
+++ src/components/BlocoParaTeste.jsx
@@ -0,0 +1,12 @@
+
+
+const BlocoParaTeste = () => {
+  return (
+    <div>
+      <h2>peça de teste</h2>
+      <p>não tem mais nada ainda</p>
+    </div>
+  )
+}
+
+export default BlocoParaTeste
---
+++ src/components/SegundoExemplo.jsx
@@ -0,0 +1,14 @@
+import "./SegundoExemplo.css";
+
+const SegundoExemplo = () => {
+  return (
+    <div>
+        <img src="https://www.comparaonline.com.br/blog-statics/br/uploads/2014/12/rsz_nevoeiro.jpg" alt="caminho" />
+      <h3>comentario vago</h3>
+      <p id="menos">não está</p>
+      <p id="mais">está sim</p>
+    </div>
+  )
+}
+
+export default SegundoExemplo
